8020c9a8ce447f522f875863b551679071ba535c
jiwon-code <ziyuan_xu@berkeley.edu>
2024-02-28 22:22:37 +0800

    IntelliJ commit 1

--- src/exceptions/ArrayIndex.java
+++ src/exceptions/ArrayIndex.java
@@ -1,7 +1,12 @@
 package exceptions;
 
 import java.util.Scanner;
+class OpenException extends Throwable{
 
+}
+class CloseException extends Throwable{
+
+}
 public class ArrayIndex {
     public static void f(){
         int[] a = new int[10];
@@ -24,11 +29,28 @@ public class ArrayIndex {
             System.out.println("k()");
         }
     }
+    public static int open(){
+        return -1;
+    }
+    public static void readFile() throws OpenException, CloseException {
+        if(open()==1);
+        throw new OpenException();
+    }
     public static void main(String[] args){
         try{
             k();
         }catch(ArrayIndexOutOfBoundsException e){
             System.out.println("Caught");
+            System.out.println(e.getMessage());
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        try {
+            readFile();
+        } catch (OpenException e) {
+            throw new RuntimeException(e);
+        } catch (CloseException e) {
+            throw new RuntimeException(e);
         }
         System.out.println("main");
     }
